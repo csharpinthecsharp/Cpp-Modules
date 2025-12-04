e7d1ddea6a9899111cdc72c2f8ece6830993c1e4
lcn <trilllucien@gmail.com>
2025-12-04 02:25:00 +0100

    ifjeifj

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,23 +1,41 @@
 #include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 Account::Account( int initial_deposit )
 {
+    _accountIndex = _nbAccounts;
+    _amount = initial_deposit;
     Account::_totalNbDeposits = initial_deposit;
+    std::cout << "[19920104_091532] " << "index:"<< _accountIndex << 
+        ";ammount:" << initial_deposit << ";created" << std::endl;
+    _nbAccounts++;
+    _totalAmount+=initial_deposit;
 }
 Account::~Account( void )
 {
-    
+
 }
 
 void	Account::makeDeposit( int deposit )
 {
     _totalAmount += deposit;
+    _nbDeposits+=1;
     _totalNbDeposits+=1;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
-    return true;
+    if (_amount >= withdrawal) {
+        _totalNbWithdrawals+=1;
+        _nbWithdrawals+=1;
+        return true;
+    }
+    return false;
 }
 
 int		Account::checkAmount( void ) const
@@ -27,7 +45,10 @@ int		Account::checkAmount( void ) const
 
 void	Account::displayStatus( void ) const
 {
-
+    // I need to replace 19920104_091532 by _displayTimestamp()
+    std::cout << "[19920104_091532] " << "index:"
+        << _accountIndex << ";amount:" << _amount << ";deposits:"
+            << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 int Account::getNbAccounts( void )
@@ -45,4 +66,16 @@ int	Account::getNbDeposits( void )
 int	Account::getNbWithdrawals( void )
 {
     return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos( void )
+{
+    std::cout << "[19920104_091532] " << "accounts:" 
+        << _nbAccounts << ";total:" << _totalAmount << ";deposits:" 
+            << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp( void )
+{
+
 }
