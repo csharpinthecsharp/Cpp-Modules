aa926b71ed17a1dafcfd67fb6616ccb7b4e7afa1
lcn <trilllucien@gmail.com>
2025-12-08 23:20:22 +0100

    test

--- Module1/ex04/main.cpp
+++ Module1/ex04/main.cpp
@@ -2,12 +2,13 @@
 
 int main(int ac, char **av) {
 	if (ac != 4) {
-		std::cout << "better_sed: 3 params required (<filename> <s1> <s2>)!"
+		std::cout << "sed: 3 params required (<filename> <s1> <s2>)!"
 			<< std::endl;
-		return 0;
+		return 1;
 	}
 	Sed sed = Sed(av[1], av[2], av[3]);
 	if (!sed.isExist())
 		return 1;
 	sed.findAndReplace();
+	return 0;
 }
