5728d16710d97af5018a5466468b721a8d6d6909
Lucien Trillard <ltrillar@c3r6s4.42luxembourg.lu>
2025-12-08 12:51:34 +0100

    Ddwdw

--- Module0/ex02/Account.cpp
+++ Module0/ex02/Account.cpp
@@ -1,5 +1,7 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -10,14 +12,16 @@ Account::Account( int initial_deposit )
 {
     _accountIndex = _nbAccounts;
     _amount = initial_deposit;
-    std::cout << "[19920104_091532] " << "index:"<< _accountIndex << 
+    _displayTimestamp();
+    std::cout << "index:"<< _accountIndex << 
         ";amount:" << initial_deposit << ";created" << std::endl;
     _nbAccounts++;
     _totalAmount+=initial_deposit;
 }
 Account::~Account( void )
 {
-    std::cout << "[19920104_091532] " << "index:"
+    _displayTimestamp();
+    std::cout << "index:"
     << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
@@ -28,7 +32,8 @@ void	Account::makeDeposit( int deposit )
     _totalAmount+=deposit;
     _nbDeposits+=1;
     _totalNbDeposits+=1;
-    std::cout << "[19920104_091532] " << "index:"
+    _displayTimestamp();
+    std::cout << "index:"
     << _accountIndex << ";p_amount:" << p_amount << ";deposit:"
         << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
 }
@@ -41,13 +46,15 @@ bool	Account::makeWithdrawal( int withdrawal )
         _nbWithdrawals+=1;
         _amount-=withdrawal;
         _totalAmount-=withdrawal;
-        std::cout << "[19920104_091532] " << "index:"
+        _displayTimestamp();
+        std::cout << "index:"
             << _accountIndex << ";p_amount:" << p_amount 
                 << ";withdrawal:"  << withdrawal << ";amount:" 
                     << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
         return true;
     }
-    std::cout << "[19920104_091532] " << "index:"
+    _displayTimestamp();
+    std::cout << "index:"
         << _accountIndex << ";p_amount:" << p_amount
             << ";withdrawal:" << "refused" << std::endl;
     return false;
@@ -60,8 +67,9 @@ int		Account::checkAmount( void ) const
 
 void	Account::displayStatus( void ) const
 {
+    _displayTimestamp();
     // I need to replace 19920104_091532 by _displayTimestamp()
-    std::cout << "[19920104_091532] " << "index:"
+    std::cout << "index:"
         << _accountIndex << ";amount:" << _amount << ";deposit:"
             << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
@@ -85,12 +93,22 @@ int	Account::getNbWithdrawals( void )
 
 void Account::displayAccountsInfos( void )
 {
-    std::cout << "[19920104_091532] " << "accounts:" 
+    _displayTimestamp();
+    std::cout << "accounts:" 
         << _nbAccounts << ";total:" << _totalAmount << ";deposit:" 
             << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void Account::_displayTimestamp( void )
 {
-
-}
+    std::time_t rawt = std::time(0);
+    std::tm *localt = std::localtime(&rawt);
+    int year = localt->tm_year + 1900;
+    int month = localt->tm_mon + 1;
+    std::cout << "[" << year << 
+        std::setfill('0') << std::setw(2) << month <<
+            std::setfill('0') << std::setw(2) << localt->tm_mday << "_" <<
+                std::setfill('0') << std::setw(2) << localt->tm_hour <<
+                    std::setfill('0') << std::setw(2) << localt->tm_min <<
+                        std::setfill('0') << std::setw(2) << localt->tm_sec << "] ";
+}   
