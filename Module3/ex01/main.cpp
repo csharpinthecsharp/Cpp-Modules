63c57247474a7c10d20def5324c684f20ed469a3
lcn <trilllucien@gmail.com>
2026-01-09 17:51:54 +0100

    Verified ex01! Ready for eval -> next is ex02

--- Module3/ex01/main.cpp
+++ Module3/ex01/main.cpp
@@ -1,22 +1,15 @@
 #include "ScavTrap.hpp"
 
 int main( int ac, char **av ) {
-	ClapTrap cp("Player0");
-	cp.attack("Alien");
-
-	ScavTrap st("Player1");
-	st.attack("Earth");
-	std::cout << "" << std::endl;
-
-	st.guardGuate();
-	std::cout << "" << std::endl;
-
-	std::cout << st << std::endl;
+	ClapTrap cp("Diego");
+	ScavTrap st("José");
 	std::cout << cp << std::endl;
-
-	ScavTrap st_copy(st);
-	std::cout << "" << std::endl;
-
-	std::cout << st_copy << std::endl;
+	std::cout << st << std::endl;
+	st.attack("Earth");
+	cp.attack("Mars");
+	std::cout << cp << std::endl;
+	std::cout << st << std::endl;
+	st.guardGuate();
+	st.guardGuate();
 	return 0;
 }
