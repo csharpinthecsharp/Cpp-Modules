ba80b9f93fafd6e431c6263c93f1dbda20dc67c8
lcn <trilllucien@gmail.com>
2026-02-04 15:33:23 +0100

    dkqzd

--- Module7/ex01/iter.hpp
+++ Module7/ex01/iter.hpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 
+template <typename T> void iter(T* array_ptr, size_t array_size, void (*f)(T const &f)) {
+    for (size_t i(0); i < array_size; ++i)
+        f(array_ptr[i]);
+}
 
-template <typename T> void iter(T* array_ptr, T* array_size) {
-    for (int i(0); i < *array_size; i++)
-        std::cout << array_ptr[i] << std::endl;
+template < typename T > void exec( T &f ) {
+    std::cout << "executed: " << f << std::endl;
 }
 
 #endif
--- Module7/ex01/main.cpp
+++ Module7/ex01/main.cpp
@@ -1,13 +1,32 @@
 #include "iter.hpp"
 
 int main( void ) {
-    int eh[] = {
+    int _i_arr[3] = {
         0,
         1,
         2
     };
-    int eh_size = 3;
+    size_t _si_array = sizeof(_i_arr) / sizeof(_i_arr[0]);
 
-    ::iter(eh, &eh_size);
+
+    std::string _s_array[5] = {
+        "f0",
+        "f1",
+        "f2",
+        "f3",
+        "f4"
+    };
+    size_t _ss_array = sizeof(_s_array) / sizeof(_s_array[0]);
+
+    const char _c_array[3] = {
+        'a',
+        'b',
+        'c',
+    };
+    size_t _cs_array = sizeof(_c_array) / sizeof(_c_array[0]);
+
+    ::iter(_i_arr, _si_array, &exec);
+    ::iter(_s_array, _ss_array, &exec);
+    ::iter(_c_array, _cs_array, &exec);
     return 0;
 }
